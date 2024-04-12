5b5fc76f9685097f964cd25ad768c38efea2c3f8
yugamthubrikar <159627260+yugamthubrikar@users.noreply.github.com>
2024-04-12 20:29:01 +0530

    Delete src directory

--- src/components/navtext.tsx
+++
@@ -1,7 +0,0 @@
-const Navtext = (props: {text:string}) => {
-    return (
-        <p className={`text-sm text-green-800 px-4 hover:text-slate-800 cursor-pointer`}>{props.text}</p>
-    );
-}
-
-export default Navtext;
